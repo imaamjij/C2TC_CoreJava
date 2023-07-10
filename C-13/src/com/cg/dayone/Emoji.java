e0db0794805d1351ae9c56a2b8d25ea92992d70a
imaam786 <imaamjij@gmail.com>
2023-07-10 09:43:55 +0530

    First Commit

---
+++ C-13/src/com/cg/dayone/Emoji.java
@@ -0,0 +1,10 @@
+package com.cg.dayone;
+
+public class Emoji {
+
+	public static void main(String[] args) {
+		System.out.println("\uD83D\uDE00");
+		System.out.println("😀");
+	}
+
+}
---
+++ C-13/src/com/cg/dayone/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.cg.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int a=5;
+		System.out.println(a);
+		System.out.println("MSAJCE");
+
+	}
+
+}
